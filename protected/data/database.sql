328a0f4e8939b060dd70b7c9bded0d24ba3defb2
Oliver Cera <olivercera@gmail.com>
2015-02-26 19:13:25 -0430

    Backup sql

--- protected/data/database.sql
+++ protected/data/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 26-02-2015 a las 21:27:26
+-- Tiempo de generación: 26-02-2015 a las 23:42:29
 -- Versión del servidor: 5.5.20
 -- Versión de PHP: 5.3.10
 
@@ -29,35 +29,34 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `categoria` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `categoria` varchar(100) NOT NULL,
-  `estado_id` int(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_categoria_estado_idx` (`estado_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id`, `categoria`, `estado_id`) VALUES
-(1, 'Carnes y Embutidos', 1),
-(2, 'Galletas y Dulces', 1),
-(3, 'Cafe, Chocolate y Panela', 1),
-(4, 'Lacteos y Jugos', 1),
-(5, 'Harinas y Avenas', 1),
-(6, 'Aceites y Vinagres', 1),
-(7, 'Condimentos, Cubitos y Especies', 1),
-(8, 'Salsas', 1),
-(9, 'Enlatados y Conservas', 1),
-(10, 'Sopas', 1),
-(11, 'Pastas', 1),
-(12, 'Arroces', 1),
-(13, 'Azucar y Sal', 1),
-(14, 'Cereales', 1),
-(15, 'Atunes y Sardines', 1),
-(16, 'Gelatinas', 1),
-(17, 'Licores', 1),
-(18, 'Gas', 1),
-(19, 'Cosmética y Cuidado Personal', 1);
+INSERT INTO `categoria` (`id`, `categoria`) VALUES
+(1, 'Carnes y Embutidos'),
+(2, 'Galletas y Gelatinas'),
+(3, 'Cafe, Chocolate y Panela'),
+(4, 'Lacteos, Jugos, Refrescos'),
+(5, 'Harinas y Avenas'),
+(6, 'Aceites y Vinagres'),
+(7, 'Condimentos, Cubitos, Especies y Sopas'),
+(8, 'Salsas'),
+(9, 'Enlatados y Conservas'),
+(11, 'Pastas y Arroces'),
+(13, 'Azucar y Sal'),
+(14, 'Cereales y Granos'),
+(17, 'Licores'),
+(18, 'Servicios'),
+(19, 'Cosmética y Aseo Personal'),
+(20, 'Hogar y Limpieza'),
+(21, 'Medicinas y Vitaminas'),
+(22, 'Articulos de Oficina'),
+(23, 'Vehiculo'),
+(24, 'Velas y Fosforos');
 
 -- --------------------------------------------------------
 
@@ -69,8 +68,7 @@ CREATE TABLE IF NOT EXISTS `detalle` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `fecha` bigint(20) NOT NULL,
   `precio` float(5,2) NOT NULL,
-  `cantidad` int(10) NOT NULL,
-  `exento` tinyint(1) DEFAULT NULL,
+  `cantidad` float(5,2) NOT NULL,
   `comentario` varchar(255) DEFAULT NULL,
   `producto_id` int(10) NOT NULL,
   `transaccion_id` int(10) NOT NULL,
@@ -82,26 +80,6 @@ CREATE TABLE IF NOT EXISTS `detalle` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estado`
---
-
-CREATE TABLE IF NOT EXISTS `estado` (
-  `id` int(1) NOT NULL AUTO_INCREMENT,
-  `estado` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Volcado de datos para la tabla `estado`
---
-
-INSERT INTO `estado` (`id`, `estado`) VALUES
-(1, 'Activo'),
-(2, 'Inactivo');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `producto`
 --
 
@@ -109,11 +87,23 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `producto` varchar(100) NOT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
-  `estado_id` int(1) NOT NULL,
+  `subcategoria_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_producto_subcategoria1_idx` (`subcategoria_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subcategoria`
+--
+
+CREATE TABLE IF NOT EXISTS `subcategoria` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `subcategoria` varchar(100) NOT NULL,
   `categoria_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_producto_estado1_idx` (`estado_id`),
-  KEY `fk_producto_categoria1_idx` (`categoria_id`)
+  KEY `fk_subcategoria_categoria1_idx` (`categoria_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -142,12 +132,6 @@ INSERT INTO `transaccion` (`id`, `transaccion`) VALUES
 --
 
 --
--- Filtros para la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD CONSTRAINT `fk_categoria_estado` FOREIGN KEY (`estado_id`) REFERENCES `estado` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Filtros para la tabla `detalle`
 --
 ALTER TABLE `detalle`
@@ -158,8 +142,13 @@ ALTER TABLE `detalle`
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD CONSTRAINT `fk_producto_categoria1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_producto_estado1` FOREIGN KEY (`estado_id`) REFERENCES `estado` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_producto_subcategoria1` FOREIGN KEY (`subcategoria_id`) REFERENCES `subcategoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `subcategoria`
+--
+ALTER TABLE `subcategoria`
+  ADD CONSTRAINT `fk_subcategoria_categoria1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
